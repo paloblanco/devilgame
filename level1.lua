66f6aa24cbecfb89d84ace49c91200d1495cf812
paloblanco <panella.rocco@gmail.com>
2021-05-20 22:23:19 -0400

    trying out flat lua files for levels

---
+++ level1.lua
@@ -0,0 +1,18 @@
+nhero = 1
+ncoin = 2
+nkey = 3
+nsentrylr = 4
+nsentryud = 5
+nportal = 6
+
+level1 = {}
+
+zone1 = {}
+layout1 = {-3,2,11,6,11} --x0,z0,dx,dy,dz
+a1 = {
+    {nhero,2,2,0}, -- nactor,dx,dy,dz
+    {ncoin,3,3,1},
+    {ncoin,4,3,1},
+    {ncoin,5,3,1},
+    {nsentrylr,3,5,0}
+}
